7334d864a448e57d8c2290d4b8405a87e46b6d32
Bazil Korath <korathbasil@live.com>
2021-11-23 01:19:45 +0530

    add settings screen items

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
-  const SettingScreen({Key? key}) : super(key: key);
+  SettingScreen({Key? key}) : super(key: key);
+
+  final menuItems = ["About", "Help", "Quit", "Add items"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Settings"),
-    );
+    return SafeArea(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "Settings",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: menuItems
+                .map((e) => Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        e,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w900, fontSize: 20),
+                      ),
+                    ))
+                .toList()),
+      ],
+    ));
   }
 }
